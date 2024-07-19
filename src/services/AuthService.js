e00beaa5a5e4540e6b7458fd2185fc4697dfb952
Frolov_AS <slaider84@list.ru>
2024-07-19 20:55:35 +0300

    19/07/2024

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -3,7 +3,10 @@ import $api from "../http/http.js";
 
 export default class AuthService {
     static async login(username, password) {
-        return $api.post('/login',{username,password}, {
+        return $api.post('/login',{
+            login:username,
+            password:password
+        }, {
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
